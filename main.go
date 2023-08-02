7f728c4e160c87f35bccbd87957da98383e8a26e
César Caycho <cfabrica46@gmail.com>
2023-08-02 00:13:56 -0500

    feat: improve clean code

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/cfabrica46/attrdet"
 	"github.com/spf13/cobra"
@@ -21,27 +21,23 @@ var (
 		Use:   "main",
 		Short: "Attribute detector",
 		Long:  `This program detects certain attributes in HTML files.`,
-		Run: func(cmd *cobra.Command, args []string) {
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if len(args) < 1 {
+				return fmt.Errorf("missing folder argument")
+			}
+
 			if (angularFlag && thymeleafFlag) || (!angularFlag && !thymeleafFlag && !scriptFlag) {
-				fmt.Println("Usage: go run main.go --angular|--thymeleaf|--scripts <folder>")
-				return
+				return fmt.Errorf("please provide at least one flag: --angular, --thymeleaf or --scripts")
 			}
 
 			folderPath := args[0]
 
-			var angularAttrs, thymeleafAttrs, angularVars, thymeleafVars map[string]int
+			angularAttrs := make(map[string]int)
+			thymeleafAttrs := make(map[string]int)
+			angularVars := make(map[string]int)
+			thymeleafVars := make(map[string]int)
 
-			if angularFlag {
-				angularAttrs = make(map[string]int)
-				angularVars = make(map[string]int)
-			}
-
-			if thymeleafFlag {
-				thymeleafAttrs = make(map[string]int)
-				thymeleafVars = make(map[string]int)
-			}
-
-			processFolder(folderPath, angularAttrs, thymeleafAttrs, angularVars, thymeleafVars)
+			return processFolder(folderPath, angularAttrs, thymeleafAttrs, angularVars, thymeleafVars)
 		},
 	}
 )
@@ -54,54 +50,63 @@ func init() {
 
 func main() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
+		fmt.Fprintln(os.Stderr, "Error:", err)
 		os.Exit(1)
 	}
 }
 
-func processFolder(folderPath string, angularAttrs, thymeleafAttrs, angularVars, thymeleafVars map[string]int) {
-	files, err := ioutil.ReadDir(folderPath)
+func processFolder(folderPath string, angularAttrs, thymeleafAttrs, angularVars, thymeleafVars map[string]int) error {
+	files, err := os.ReadDir(folderPath)
 	if err != nil {
-		fmt.Println("Error reading folder:", err)
-		return
+		return fmt.Errorf("failed to read folder: %w", err)
 	}
 
 	for _, file := range files {
+		path := filepath.Join(folderPath, file.Name())
+
 		if file.IsDir() {
-			processFolder(filepath.Join(folderPath, file.Name()), angularAttrs, thymeleafAttrs, angularVars, thymeleafVars)
+			if err = processFolder(path, angularAttrs, thymeleafAttrs, angularVars, thymeleafVars); err != nil {
+				return err
+			}
 		} else {
-			processFile(filepath.Join(folderPath, file.Name()), angularAttrs, thymeleafAttrs, angularVars, thymeleafVars)
+			if err = processFile(path, angularAttrs, thymeleafAttrs, angularVars, thymeleafVars); err != nil {
+				return err
+			}
 		}
 	}
+
+	return nil
 }
 
-func processFile(filePath string, angularAttrs, thymeleafAttrs, angularVars, thymeleafVars map[string]int) {
-	if filepath.Ext(filePath) != ".html" && filepath.Ext(filePath) != ".mst" {
-		return
+func processFile(filePath string, angularAttrs, thymeleafAttrs, angularVars, thymeleafVars map[string]int) error {
+	switch filepath.Ext(filePath) {
+	case ".html", ".mst":
+		// continue
+	default:
+		return nil
 	}
 
 	file, err := os.Open(filePath)
 	if err != nil {
-		fmt.Println("Error opening file:", err)
-		return
+		return fmt.Errorf("failed to open file: %w", err)
 	}
 	defer file.Close()
 
 	doc, err := html.Parse(bufio.NewReader(file))
 	if err != nil {
-		fmt.Println("Error parsing HTML:", err)
-		return
+		return fmt.Errorf("failed to parse HTML: %w", err)
 	}
 
-	fileResult := fmt.Sprintf("File: %s\n", filePath)
+	var results []string
+	results = append(results, fmt.Sprintf("File: %s\n", filePath))
 
 	if angularFlag {
 		angularDetector := &attrdet.AngularDetector{BaseDetector: attrdet.BaseDetector{}}
 		angularDetector.DetectAttributes(doc, angularAttrs)
 		angularDetector.DetectVariables(doc, angularVars)
 
-		fileResult += formatAttributes("Angular (ng) attributes:", angularAttrs)
-		fileResult += formatVariables("Angular variables:", angularVars)
+		results = append(results, formatAttributes("Angular (ng) attributes:", angularAttrs))
+		results = append(results, formatVariables("Angular variables:", angularVars))
 	}
 
 	if thymeleafFlag {
@@ -109,42 +114,54 @@ func processFile(filePath string, angularAttrs, thymeleafAttrs, angularVars, thy
 		thymeleafDetector.DetectAttributes(doc, thymeleafAttrs)
 		thymeleafDetector.DetectVariables(doc, thymeleafVars)
 
-		fileResult += formatAttributes("Thymeleaf attributes:", thymeleafAttrs)
-		fileResult += formatVariables("Thymeleaf variables:", thymeleafVars)
+		results = append(results, formatAttributes("Thymeleaf attributes:", thymeleafAttrs))
+		results = append(results, formatVariables("Thymeleaf variables:", thymeleafVars))
 	}
 
 	if scriptFlag {
-		scriptDetector := &attrdet.BaseDetector{}
+		scriptDetector := attrdet.BaseDetector{}
 		scriptCount := scriptDetector.DetectScriptTags(doc)
 
-		fileResult += fmt.Sprintf("Number of <script> tags: %d\n", scriptCount)
+		results = append(results, fmt.Sprintf("Number of <script> tags: %d\n", scriptCount))
 	}
 
-	fileResult += "=====\n"
-	fmt.Println(fileResult)
+	results = append(results, "=====\n")
+	fmt.Println(strings.Join(results, ""))
+
+	return nil
 }
 
 func formatAttributes(title string, attrs map[string]int) string {
-	var result string
-	if len(attrs) > 0 {
-		result += fmt.Sprintf("%s\n", title)
-		for attr, count := range attrs {
-			result += fmt.Sprintf("  %s: %d occurrences\n", attr, count)
-		}
-		result += "\n"
+	if len(attrs) == 0 {
+		return ""
 	}
+
+	var result string
+	result += fmt.Sprintf("%s\n", title)
+
+	for attr, count := range attrs {
+		result += fmt.Sprintf("  %s: %d occurrences\n", attr, count)
+	}
+
+	result += "\n"
+
 	return result
 }
 
 func formatVariables(title string, variables map[string]int) string {
-	var result string
-	if len(variables) > 0 {
-		result += fmt.Sprintf("%s\n", title)
-		for variable, count := range variables {
-			result += fmt.Sprintf("  %s: %d occurrences\n", variable, count)
-		}
-		result += "\n"
+	if len(variables) == 0 {
+		return ""
 	}
+
+	var result string
+	result += fmt.Sprintf("%s\n", title)
+
+	for variable, count := range variables {
+		result += fmt.Sprintf("  %s: %d occurrences\n", variable, count)
+	}
+
+	result += "\n"
+
 	return result
 }
 
